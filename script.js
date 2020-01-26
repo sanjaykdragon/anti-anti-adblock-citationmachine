988bff7af48790032429a1ad0f50cc0dc9d0bd4f
Sanjay K <sanjaykdragon@users.noreply.github.com>
2020-01-26 17:01:53 -0500

    added regex

--- script.js
+++ script.js
@@ -15,8 +15,12 @@ function get_element_by_xpath(path) {
 
 function format_path(num) {
     var path = "/html/body/div[" + num.toString() + "]";
-    console.log(path);
     return get_element_by_xpath(path);
+}
+
+function test_regex(to_test) {
+    var regex_fn = /^_\w{8}-\w{4}-\w{4}-\w{4}-\w{12}$/;
+    return regex_fn.test(to_test);
 }
 
 function delete_anti_adblock() {
@@ -28,8 +32,7 @@ function delete_anti_adblock() {
     var element_to_delete = format_path(iter);
     while(element_to_delete != null && element_to_delete != undefined) {
         var element_id = element_to_delete.id.toString();
-        console.log(element_id);
-        if(element_id.includes("-") && !element_id.includes("sticky") && !element_id.includes("row") && !element_id.includes("div")) { //should probably do this with regex instead
+        if(test_regex(element_id)) { //should probably do this with regex instead
             console.log("Removed popup: " + element_id);
             element_to_delete.remove();
             should_run = false;
